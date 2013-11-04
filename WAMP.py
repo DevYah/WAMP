0af5d16096027e9ff789a504f4be471d6fa35876
DevYah <yahiaelgamal@gmail.com>
2013-11-04 22:22:12 +0200

    Add greedy search

--- WAMP.py
+++ WAMP.py
@@ -1,8 +1,9 @@
 from ADTs import State, SearchNode, SearchProblem
 from random import random, randint
-from pprint import pprint, pformat
-from copy import deepcopy, copy
-from search_queues import BFS_Queue, DFS_Queue
+#from pprint import pprint, pformat
+from copy import deepcopy
+from search_queues import BFS_Queue, DFS_Queue, BestFirst_Queue
+from heuristics import heuristic1, heuristic2
 
 
 class Part:
@@ -32,7 +33,7 @@ class Part:
         if self.can_assemble(other):
             return Part(self.locations + other.locations)
         else:
-            print 'WHAT THE FUCK YOU WANT'
+            raise Exception('Un-assembleable parts')
 
 
 class Grid(State):
@@ -185,7 +186,7 @@ class Grid(State):
             new_loc = Grid.apply_direction(location, direction)
             new_locations.append(new_loc)
             if self.grid[new_loc[0]][new_loc[1]] != '_':
-                print 'WAHAAAAAAAAAAAAAAAAAAAAAAAAAAT'
+                raise Exception('inapplicable move')
 
             self.grid[new_loc[0]][new_loc[1]] = 'R'
             self.grid[location[0]][location[1]] = '_'
@@ -236,7 +237,7 @@ class WAMP_SearchNode(SearchNode):
         nodes = []
         for operator in operators:
             new_state, feedback = self.state.apply_operator(operator)
-            cost = 1  # FIXME make it depend on the feedback
+            cost = 1  # TODO properly calculate it
             if feedback != 'damage':
                 new_node = WAMP_SearchNode(new_state,
                                            parent_node=self,
@@ -290,7 +291,7 @@ class WAMP_SearchProblem(SearchProblem):
         return node.expand()
 
 
-def search(grid, strategy, visualize):
+def search(grid, strategy, visualize=False):
     search_problem = WAMP_SearchProblem(grid)
     if strategy == 'BFS':
         return bfs(search_problem, visualize)
@@ -307,42 +308,20 @@ def search(grid, strategy, visualize):
     elif strategy == 'AS2':
         return AS2(search_problem, visualize)
     else:
-        raise Exception('Gayeen nehazar!!? choose a proper strategy')
+        raise Exception('%s is not a supported strategy' % strategy)
 
 
-def bfs(search_problem, visualize):
-    expanded_nodes_count = 0
-    start_node = WAMP_SearchNode(search_problem.initial_state)
+def bfs(search_problem, visualize=False):
     nodes_q = BFS_Queue()
-    nodes_q.enqueue([start_node])
-    while True:
-        if len(nodes_q) == 0:
-            return [None, None, expanded_nodes_count]
-        node = nodes_q.remove_front()
-        print 'len(nodes_q): %d, depth: %d' % (len(nodes_q), node.depth)
-        expanded_nodes_count += 1
-        if search_problem.goal_test(node.state):
-            return [node.path_repr(), 'computed_cost', expanded_nodes_count]
-        nodes_q.enqueue(node.expand())
+    return general_search(search_problem, nodes_q)
 
 
-def dfs(search_problem, visualize):
-    expanded_nodes_count = 0
-    start_node = WAMP_SearchNode(search_problem.initial_state)
+def dfs(search_problem, visualize=False):
     nodes_q = DFS_Queue()
-    nodes_q.enqueue([start_node])
-    while True:
-        if len(nodes_q) == 0:
-            return [None, None, expanded_nodes_count]
-        node = nodes_q.remove_front()
-        print 'len(nodes_q): %d, depth: %d' % (len(nodes_q), node.depth)
-        expanded_nodes_count += 1
-        if search_problem.goal_test(node.state):
-            return [node.path_repr(), 'computed_cost', expanded_nodes_count]
-        nodes_q.enqueue(node.expand())
+    return general_search(search_problem, nodes_q)
 
 
-def ID(search_problem, visualize):
+def ID(search_problem, visualize=False):
     depth_limit = 1
     expanded_nodes_count = 0
     start_node = WAMP_SearchNode(search_problem.initial_state)
@@ -371,33 +350,40 @@ def ID(search_problem, visualize):
         nodes_q.enqueue(node.expand())
 
 
-def GR1(grid, visualize):
+def GR1(search_problem, visualize=False):
+    return greedy(search_problem, heuristic1, visualize)
+
+
+def GR2(search_problem, visualize=False):
+    return greedy(search_problem, heuristic2, visualize)
+
+
+def greedy(search_problem, heuristic_func, visualize=False):
+    nodes_q = BestFirst_Queue(heuristic_func)
+    return general_search(search_problem, nodes_q)
+
+
+def AS1(search_problem, visualize=False):
     pass
 
 
-def GR2(grid, visualize):
-    pass
-
-
-def AS1(grid, visualize):
-    pass
-
-
-def AS2(grid, visualize):
+def AS2(search_problem, visualize=False):
     pass
 
 
 def general_search(search_problem, nodes_q):
+    expanded_nodes_count = 0
     start_node = WAMP_SearchNode(search_problem.initial_state)
     nodes_q.enqueue([start_node])
     while True:
         if len(nodes_q) == 0:
-            return False
+            return [None, None, expanded_nodes_count]
         node = nodes_q.remove_front()
-        #print "%d %d" % (node.depth, len(nodes_q))
+        print 'len(nodes_q): %d, depth: %d' % (len(nodes_q), node.depth)
+        expanded_nodes_count += 1
         if search_problem.goal_test(node.state):
-            return node
-        nodes_q.enqueue(search_problem.expand_node(node))
+            return [node.path_repr(), 'computed_cost', expanded_nodes_count]
+        nodes_q.enqueue(node.expand())
 
 
 def run():
@@ -409,30 +395,3 @@ def run():
     # nodes_q = BFS_Queue()
     nodes_q = BFS_Queue()
     return general_search(search_problem, nodes_q)
-
-
-#node = run()
-#node.print_path()
-
-#g_ar = [['_', 'R', '_', '_'],
-        #['R', '_', 'R', '_'],
-        #['_', '_', '_', '_'],
-        #['_', 'X', '_', '_']]
-
-#g = Grid(g_ar)
-#print g
-#print '--------------'
-#operator = (1, 'E')
-#g2,fb = g.apply_operator(operator)
-#print g2
-#print fb
-#operator = (0, 'S')
-#g3,fb = g2.apply_operator(operator)
-#print g3
-#print fb
-#operator = (0, 'E')
-#g4,fb = g3.apply_operator(operator)
-#print g4
-#print fb
-##g2 = g.apply_operator(operator)
-##print g2
--- search_queues.py
+++ search_queues.py
@@ -1,4 +1,5 @@
 from ADTs import SearchQueue
+from heapq import heappush, heappop
 
 
 class BFS_Queue(SearchQueue):
@@ -9,6 +10,7 @@ class BFS_Queue(SearchQueue):
         s = "BFS Q"
         s += format(self.q)
         return s
+
     def __len__(self):
         return len(self.q)
 
@@ -18,6 +20,7 @@ class BFS_Queue(SearchQueue):
 
     def remove_front(self):
         return self.q.pop(0)
+
 
 class DFS_Queue(SearchQueue):
     def __init__(self):
@@ -37,3 +40,24 @@ class DFS_Queue(SearchQueue):
 
     def remove_front(self):
         return self.q.pop()
+
+
+class BestFirst_Queue(SearchQueue):
+    def __init__(self, cost_function):
+        self.q = []
+        self.cost_function = cost_function
+
+    def __str__(self):
+        s = "BestFrist Q"
+        s += format(self.q)
+        return s
+
+    def __len__(self):
+        return len(self.q)
+
+    def enqueue(self, nodes):
+        for node in nodes:
+            heappush(self.q, (self.cost_function(node), node))
+
+    def remove_front(self):
+        return heappop(self.q)[1]
